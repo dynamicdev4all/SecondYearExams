90f4c50201883c220e273cb9d35aac5c60a27903
dynamicdev4all <149931467+dynamicdev4all@users.noreply.github.com>
2025-06-13 17:24:15 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+class Student {
+    public static void main(String[] args) {
+        // int p [] = new int[10];
+        // int j [] = {1,2,3};
+
+        // for(int i = 1;i<j.length;i++){
+        // System.out.println(j[i]);
+        // }
+        // for(int i : j){
+        // System.out.println(i);
+        // }
+
+        // ArrayList<String > list = new ArrayList<>();
+        // System.out.println(list.size());
+        // // System.out.println();
+        // list.add("SECA");
+        // System.out.println(list.size());
+
+        String name = "vansh";
+        String name2 ="V -ansh";
+        System.out.println(name.equals(name2));
+        System.out.println(name.compareTo(name2));
+
+    }
+}
